b4d5cd0f3b61236503b28e9b97d80675795635cd
Michael Low <mikelow@gmail.com>
2021-01-15 23:02:10 +0700

    Improve header responsive layout

--- src/shared-components/header.tsx
+++ src/shared-components/header.tsx
@@ -28,77 +28,67 @@ const GeneralHeader = ({ buttonColor = 'red', openMobileMenu = () => {} }: Props
     <section className="container py-3">
       <div className="flex items-center justify-between">
         <Link href="/">
-          <a className="mr-auto next-image-wrapper" style={{ maxWidth: '302px' }}>
-            <NextImage src="/images/mini-header/logo.png" alt="Logo" width="604" height="89" />
-          </a>
+          <>
+            <a className="mr-auto next-image-wrapper hidden lg:block" style={{ maxWidth: '302px' }}>
+              <NextImage src="/images/mini-header/logo.png" alt="Logo" width="604" height="89" />
+            </a>
+            <a className="mr-auto next-image-wrapper lg:hidden">
+              <NextImage src="/images/mini-header/logo.png" alt="Logo" width="272" height="40" />
+            </a>
+          </>
         </Link>
-        <Link href="/course">
-          <a className="mx-7 text-gray-600 hover:text-red-600 text-lg">
-            <Button size="md" bgColor={buttonColor}>
-              Video Course
-            </Button>
-          </a>
-        </Link>
-
         <div className="hidden lg:block">
+          <Link href="/course">
+            <a className="mx-7 text-gray-600 hover:text-red-600 text-lg">
+              <Button size="md" bgColor={buttonColor}>
+                Video Course
+              </Button>
+            </a>
+          </Link>
+        </div>
+
+        <div className="lg:hidden">
+          <i onClick={openMobileMenu} className="material-icons text-3xl text-gray-600">
+            menu
+          </i>
+        </div>
+
+        <div className="hidden lg:flex ">
           {!isSearchBoxVisible && (
-            <span
-              className={'cursor-pointer ' + (isSearchBoxVisible ? 'invisible' : 'ml-7')}
+            <i
+              className={
+                'material-icons text-3xl text-gray-600 cursor-pointer ' +
+                (isSearchBoxVisible ? 'invisible' : 'ml-7')
+              }
               onClick={() => setIsSearchBoxVisible(true)}
             >
-              <i className="material-icons" style={{ top: '0px' }}>
-                search
-              </i>
-            </span>
+              search
+            </i>
           )}
 
-          <form
-            action="/search"
-            className={isSearchBoxVisible ? 'search-bar visible' : 'search-bar'}
-          >
-            <input
-              ref={searchBoxRef}
-              autoComplete="off"
-              defaultValue=""
-              type="text"
-              name="q"
-              placeholder="Search"
-            />
-          </form>
+          {isSearchBoxVisible && (
+            <form
+              action="/search"
+              className={
+                'search-bar p-0 flex justify-center items-center overflow-hidden h-10 w-0 ' +
+                (isSearchBoxVisible ? 'w-52' : '')
+              }
+            >
+              <input
+                ref={searchBoxRef}
+                autoComplete="off"
+                defaultValue=""
+                type="text"
+                name="q"
+                placeholder="Search"
+                className="outline-none rounded-2xl text-base py-1 px-4 text-gray-600 border border-gray-300 w-48"
+              />
+            </form>
+          )}
         </div>
       </div>
 
       <style jsx>{`
-        .search-bar {
-          overflow: hidden;
-          will-change: width;
-          transition: width 0.4s, height 0.2s;
-          padding: 0;
-          display: inline-block;
-          width: 0;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        .search-bar.visible {
-          width: 230px;
-          height: 40px;
-        }
-        input,
-        input:focus,
-        input:active {
-          padding: 8px 12px;
-          font-size: 17px;
-          width: 220px;
-          color: #5f5f5f;
-          outline: none;
-          border: 1px solid #ccc;
-          -webkit-appearance: none;
-          max-height: 34px;
-          border-radius: 15px;
-          box-shadow: none;
-          transition: box-shadow 0.2s;
-        }
         input:active,
         input:focus {
           box-shadow: 0 0 2px 2px #88bfff;
@@ -106,9 +96,6 @@ const GeneralHeader = ({ buttonColor = 'red', openMobileMenu = () => {} }: Props
         input::placeholder {
           color: #ccc;
           font-style: italic;
-          border-radius: 0px;
-          border: 0px;
-          outline: none;
         }
       `}</style>
     </section>
