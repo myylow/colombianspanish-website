c77348d6c327173280988438753a730065962816
Michael Low <mikelow@gmail.com>
2021-01-24 18:08:25 +0700

    Add custom document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+
+class ColombianSpanishDocument extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default ColombianSpanishDocument
